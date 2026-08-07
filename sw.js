ee9364db47329e402b5945cd9772df82200ade20
sbouthillette <sbouthillette@groupesbc.com>
2026-08-07 02:38:27 +0000

    Updated the layout of the Warehouse designer

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js — basic offline app-shell cache for Warehouse Builder PWA
-const CACHE_NAME = 'warehouse-builder-v4';
+const CACHE_NAME = 'warehouse-builder-v5';
 const APP_SHELL = [
   './',
   './index.html',
